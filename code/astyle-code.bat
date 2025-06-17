515f9c522a23462743d1fcfc336dc04177d58c51
Robert Beckebans <robert.beckebans@gmail.com>
2025-06-17 10:49:12 +0200

    Synced formatters with RBDOOM-3-BFG clang-format-update

--- code/astyle-code.bat
+++ code/astyle-code.bat
@@ -1,12 +1,12 @@
-REM astyle.exe -v --formatted --options=astyle-options.ini --exclude="code/libs" --recursive *.h
-REM astyle.exe -v --formatted --options=astyle-options.ini --exclude="code/libs" --recursive *.c
-REM astyle.exe -v --formatted --options=astyle-options.ini --exclude="code/libs" --recursive *.cpp
-REM astyle.exe -v --formatted --options=astyle-options.ini --exclude="code/libs" --recursive *.glsl
-REM astyle.exe -v --formatted --options=astyle-options.ini --exclude="code/libs" --recursive *.comp
-REM astyle.exe -v --formatted --options=astyle-options.ini --exclude="code/libs" --recursive *.vert
-REM astyle.exe -v --formatted --options=astyle-options.ini --exclude="code/libs" --recursive *.frag
+REM astyle.exe -v --formatted --options=astyle-header.ini --exclude="code/libs" --recursive *.h
+REM astyle.exe -v --formatted --options=astyle-cpp.ini --exclude="code/libs" --recursive *.c
+REM astyle.exe -v --formatted --options=astyle-cpp.ini --exclude="code/libs" --recursive *.cpp
+REM astyle.exe -v --formatted --options=astyle-cpp.ini --exclude="code/libs" --recursive *.glsl
+REM astyle.exe -v --formatted --options=astyle-cpp.ini --exclude="code/libs" --recursive *.comp
+REM astyle.exe -v --formatted --options=astyle-cpp.ini --exclude="code/libs" --recursive *.vert
+REM astyle.exe -v --formatted --options=astyle-cpp.ini --exclude="code/libs" --recursive *.frag
 
-astyle.exe -v --formatted --options=astyle-options.ini shared\q_shared.h
-astyle.exe -v --formatted --options=astyle-options.ini engine\renderer\tr_local.h
+astyle.exe -v --formatted --options=astyle-header.ini shared\q_shared.h
+astyle.exe -v --formatted --options=astyle-header.ini engine\renderer\tr_local.h
 
 pause
